57000536c042687f1104238c00dfdfb936c3b21f
Satwik K <satwik03112006@gmail.com>
2026-07-11 15:01:00 +0530

    Backend initial implementation

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import shutil
 
 app = FastAPI(
     title="SBOM Sentinel",
@@ -16,16 +18,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Create uploads folder if it doesn't exist
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
 @app.get("/")
 def home():
     return {
         "message": "Welcome to SBOM Sentinel 🚀"
     }
 
+
 @app.get("/health")
 def health():
     return {
-        "status": "Backend Running",
-        "project": "SBOM Sentinel",
-        "version": "1.0.0"
+        "status": "Backend Running"
+    }
+
+
+@app.get("/dashboard")
+def dashboard():
+    return {
+        "security_score": 82,
+        "vulnerabilities": 5,
+        "license_issues": 2,
+        "dependencies": 50
+    }
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "File uploaded successfully!",
+        "filename": file.filename
     }
